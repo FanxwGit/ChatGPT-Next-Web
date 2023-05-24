b5d3fa1502df11877f1a8a7aa1a1e57b457ab096
oukGit <fanxuwei@foxmail.com>
2023-05-25 02:31:41 +0800

    feat:add some mask

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -60,6 +60,29 @@ export const CN_MASKS: BuiltinMask[] = [
     builtin: true,
   },
   {
+    avatar: "1f354",
+    name: "中文学术论文写作改进助理",
+    context: [
+      {
+        role: "user",
+        content:
+          "As a Chinese academic paper writing improvement assistant, your task is toimprove the spelling,grammar,clarity,conciseness and overall readability of the text provided,while breaking down long sentences,reducing repetition,andproviding improvement suggestions. Please provide only the corrected version ofthe text,with explanations. Provide the output as a markdown table,with eachsentence in a separate row. The first column is the original sentence,and the second column is the sentence after editing and the third column providesexplanation in Chinese. Please edit the following text.",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 1,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      sendMemory: true,
+      historyMessageCount: 4,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+  },
+  {
     avatar: "1f978",
     name: "机器学习",
     context: [
